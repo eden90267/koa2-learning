b76450ea6b1fedfc4c527a48846e265182610eea
Eden Liu <eden90267@gmail.com>
2017-07-19 18:03:47 +0800

    Impl Demo

---
+++ demo/config.js
@@ -0,0 +1,12 @@
+const config = {
+    port: 3001,
+    database: {
+        DATABASE: 'koa-demo',
+        USERNAME: 'root',
+        PASSWORD: '1234',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+};
+
+module.exports = config;
